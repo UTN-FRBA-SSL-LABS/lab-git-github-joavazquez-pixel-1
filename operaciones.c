5a0c7772a760039b19093a42a839258e079f441b
JoaquÍn Eduardo VÁzquez <joavazquez@frba.utn.edu.ar>
2026-05-01 18:43:03 -0300

    Update operaciones.c

--- operaciones.c
+++ operaciones.c
@@ -15,8 +15,7 @@ int restar(int a, int b) {
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 
 int multiplicar(int a, int b) {
-    return a*b; 
-    //multiplicar retorna el valor de la variable a multiplicado por el valor de la variable b
+    return a*b;/*multiplicar retorna el valor de la variable a multiplicado por el valor de la variable b*/
 }
 
 /* ── esPar ────────────────────────────────────────────────────────────────── */
